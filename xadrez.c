a34f4407a9ed56d26dc1edf1592061be73ca1556
JPatit <joaopatomoscardini@gmail.com>
2025-04-12 03:40:08 +0000

    finalizado

--- xadrez.c
+++ xadrez.c
@@ -1,46 +1,68 @@
 #include <stdio.h>
 
-int main(){
+//void da torre
+void Torre(int T) {
+    if (T == 0) return;
+    printf("Direita\n");
+    Torre(T - 1);
+}
 
-    //movendo o bispo usando a estrutura de repetição "for"
-    for (int b = 0; b < 5; b++){
-        printf("Bispo anda para cima e direita!\n");
-    }
-
-    printf("\n");
-
-    //movendo torre usando a estrutura "while"
-    int t = 0;
-
-    while (t < 5){
-        printf("Torre anda para direita!\n");
-        t++;
-    }
-    
-    printf("\n");
-
-    //movendo rainha com a estrutura "do-while"
-    int r = 0;
-
-    do{
-        printf("Rainha anda para esquerda\n");
-        r++;
-    }   while (r < 8);
-
-    printf("\n");
-
-    //por fim, movendo o cavalo usando for interno e while externo
-
-    int movimento = 1;
-
-    while (movimento--){
-        
-        for (int c = 0; c < 2; c++){
-            printf("Cavalo anda para baixo\n");
+//void do bispo
+void Bispo(int B) {
+    for (int i = 0; i < B; i++) {
+        for (int j = 0; j < 1; j++) {
+            printf("Cima e Esquerda\n");
         }
-        printf("Cavalo anda para esquerda\n");
-
     }
-    
+}
+
+//void da rainha
+void Rainha(int R) {
+    if (R == 0) return;
+    printf("Esquerda\n");
+    Rainha(R - 1);
+}
+
+//void do cavalo
+void Cavalo(int C1, int C2) {
+    for (int i = 0; i < C1; i++) {
+        for (int j = 0; j < C2; j++) {
+            if (i + 2 < C1 && j + 1 < C2) {
+                printf("Cima\n");
+                printf("Cima\n");
+                printf("Direita\n");
+                return;
+            }
+        }
+    }
+}
+
+int main() {
+    int movertorre = 5;
+    int moverbispo = 5;
+    int moverrainha = 8;
+    int movercavalo1 = 8;
+    int movercavalo2 = 8;
+
+    // Torre
+    printf("Torre anda para:\n");
+    Torre(movertorre);
+    printf("\n");
+
+    // Bispo
+    printf("Bispo anda para:\n");
+    Bispo(moverbispo);
+    printf("\n");
+
+    // Rainha
+    printf("Rainha anda para:\n");
+    Rainha(moverrainha);
+    printf("\n");
+
+    // Cavalo
+    printf("Cavalo anda para:\n");
+    Cavalo(movercavalo1, movercavalo2);
+    printf("\n");
+
     return 0;
 }
